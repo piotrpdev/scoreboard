a0bf0a948a9d3b456c67747ccbb5925a3eae424f
Piotr Płaczek <piotrpdev@gmail.com>
2026-01-11 16:07:22 +0000

    Minor clean-ups

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,18 @@
-use std::sync::LazyLock;
-
 use esp_idf_svc::{
     hal::{
         delay::FreeRtos,
         gpio::{self, Gpio0, PinDriver},
         prelude::*,
-        spi::{self, SpiDeviceDriver, SpiDriver},
+        spi::{SpiConfig, SpiDeviceDriver, SpiDriver, config::DriverConfig},
     },
     log::EspLogger,
 };
 
-use anyhow::{anyhow, Result};
+use anyhow::{Context, Result, anyhow};
 
 use embedded_graphics::{
     image::{Image, ImageRaw, ImageRawLE},
-    pixelcolor::{raw::LittleEndian, Rgb565},
+    pixelcolor::{Rgb565, raw::LittleEndian},
     prelude::*,
 };
 
@@ -27,11 +25,10 @@ const DISPLAY_HEIGHT: u32 = 160;
 const DISPLAY_USE_RGB: bool = true;
 const DISPLAY_INVERT_COLORS: bool = false;
 
-static RAW_FERRIS_IMAGE: LazyLock<ImageRawLE<'static, Rgb565>> =
-    LazyLock::new(|| ImageRaw::new(include_bytes!("../assets/ferris.raw"), 86));
-
-static FERRIS_IMAGE: LazyLock<Image<'static, ImageRaw<'static, Rgb565, LittleEndian>>> =
-    LazyLock::new(|| Image::new(&RAW_FERRIS_IMAGE, Point::new(26, 8)));
+static RAW_FERRIS_IMAGE: ImageRawLE<'static, Rgb565> =
+    ImageRaw::new(include_bytes!("../assets/ferris.raw"), 86);
+static FERRIS_IMAGE: Image<'static, ImageRaw<'static, Rgb565, LittleEndian>> =
+    Image::new(&RAW_FERRIS_IMAGE, Point::new(26, 8));
 
 type Spi<'a> = SpiDeviceDriver<'a, SpiDriver<'a>>;
 type DcPin<'a> = PinDriver<'a, gpio::Gpio2, gpio::Output>;
@@ -40,27 +37,26 @@ type RstPin<'a> = PinDriver<'a, gpio::Gpio3, gpio::Output>;
 type Display<'a> = ST7735<Spi<'a>, DcPin<'a>, RstPin<'a>>;
 
 fn create_display_instance() -> Result<Display<'static>> {
-    let peripherals = Peripherals::take()?;
+    let peripherals =
+        Peripherals::take().context("Failed to take peripherals when creating display instance")?;
 
     let spi = peripherals.spi2;
     let sclk = peripherals.pins.gpio8;
     let sdo = peripherals.pins.gpio4;
-    let sdi = Option::<Gpio0>::None;
-    let cs = Some(peripherals.pins.gpio1);
+    let cs = peripherals.pins.gpio1;
 
-    let spi_config = spi::SpiConfig::new().baudrate(SPI_BAUDRATE_MHZ.MHz().into());
-
-    let spi = spi::SpiDeviceDriver::new_single(
+    let spi = SpiDeviceDriver::new_single(
         spi,
         sclk,
         sdo,
-        sdi,
-        cs,
-        &Default::default(),
-        &spi_config,
+        Option::<Gpio0>::None,
+        Some(cs),
+        &DriverConfig::default(),
+        &SpiConfig::new().baudrate(SPI_BAUDRATE_MHZ.MHz().into()),
     )?;
 
-    let rst = PinDriver::output(peripherals.pins.gpio3)?;
+    let rst =
+        PinDriver::output(peripherals.pins.gpio3).context("Failed to create RST pin driver")?;
     let dc = PinDriver::output(peripherals.pins.gpio2)?;
 
     let display = ST7735::new(
@@ -84,22 +80,22 @@ fn main() -> Result<()> {
     // Bind the log crate to the ESP Logging facilities
     EspLogger::initialize_default();
 
-    let mut display = create_display_instance()?;
+    let mut display = create_display_instance().context("Failed to create display instance")?;
 
     display
         .init(&mut FreeRtos)
-        .map_err(|_| anyhow!("Failed to initialize display"))?;
+        .map_err(|()| anyhow!("Failed to initialize display"))?;
     display
         .clear(Rgb565::BLACK)
-        .map_err(|_| anyhow!("Failed to clear display"))?;
+        .map_err(|()| anyhow!("Failed to clear display"))?;
     display
         .set_orientation(&Orientation::LandscapeSwapped)
-        .map_err(|_| anyhow!("Failed to set orientation"))?;
+        .map_err(|()| anyhow!("Failed to set display orientation"))?;
     display.set_offset(0, 25);
 
     FERRIS_IMAGE
         .draw(&mut display)
-        .map_err(|_| anyhow!("Failed to draw image"))?;
+        .map_err(|()| anyhow!("Failed to draw image on display"))?;
 
     info!("LCD test done.");
     loop {
